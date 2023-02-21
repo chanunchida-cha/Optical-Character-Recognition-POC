e095099f0eaab02029fd1147baf5623bc334ddb6
Jacky <jacky.teerapat@gmail.com>
2023-02-21 10:47:59 +0700

    test reduce

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,49 +6,51 @@ import Resizer from "react-image-file-resizer";
 interface Props {}
 
 function Index({}: Props): ReactElement {
-  const [image, setImage] = useState<File>();
-  const [preview, setPreview] = useState<string>();
+    const [image, setImage] = useState<File>();
+    const [preview, setPreview] = useState<string>();
 
-  // const resizer = async (file: File) => {
-  //   const resizeFile = new Promise((resolve, reject) => {
-  //     Resizer.imageFileResizer(
-  //       file,
-  //       500, ////width
-  //       500, /// height
-  //       "WEBP",
-  //       100, /// quality
-  //       0, // Rotate degree
-  //       (uri: any) => {
-  //         resolve(uri);
-  //       },
-  //       "file"
-  //     );
-  //   }).then((res) => res);
-  //   const resizeFiled = await Promise.all([resizeFile]).then((res) => res[0]);
+    const resizer = async (file: File) => {
+        const resizeFile = new Promise((resolve, reject) => {
+            Resizer.imageFileResizer(
+                file,
+                500, ////width
+                500, /// height
+                "WEBP",
+                100, /// quality
+                0, // Rotate degree
+                (uri: any) => {
+                    resolve(uri);
+                },
+                "file"
+            );
+        }).then((res) => res);
+        const resizeFiled = await Promise.all([resizeFile]).then(
+            (res) => res[0]
+        );
 
-  //   return resizeFiled;
-  // };
+        return resizeFiled;
+    };
 
-  // useEffect(() => {
-  //   console.log(image);
-  //   if (!image) return;
-  //   resizer(image).then((imgResize) => {
-  //     console.log("imgResize", imgResize);
-  //     ///do somthing with file
-  //   });
-  //   if (!image) return;
-  // }, [image]);
+    useEffect(() => {
+        console.log(image);
+        if (!image) return;
+        resizer(image).then((imgResize) => {
+            console.log("imgResize", imgResize);
+            ///do somthing with file
+        });
+        if (!image) return;
+    }, [image]);
 
-  return (
-    <div className=" mx-5 sm:mx-auto sm:w-2/3 mt-6  object-none">
-      <FormMed
-        image={image!}
-        preview={preview!}
-        setPreview={setPreview}
-        setImage={setImage}
-      />
-    </div>
-  );
+    return (
+        <div className=" mx-5 sm:mx-auto sm:w-2/3 mt-6  object-none">
+            <FormMed
+                image={image!}
+                preview={preview!}
+                setPreview={setPreview}
+                setImage={setImage}
+            />
+        </div>
+    );
 }
 
 export default Index;
