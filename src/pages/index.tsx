ef9c51b8f88b48ecd89480a99fad9fbde92a462b
Jacky <jacky.teerapat@gmail.com>
2023-02-21 11:01:22 +0700

    70, /// quality

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,7 +16,7 @@ function Index({}: Props): ReactElement {
                 500, ////width
                 500, /// height
                 "WEBP",
-                80, /// quality
+                70, /// quality
                 0, // Rotate degree
                 (uri: any) => {
                     resolve(uri);
